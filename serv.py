b41b0181c6cf67c6b895b9ccb36be9cb45107ae4
PashmiDev <lowneonema@gmail.com>
2020-06-23 12:27:45 +0200

    w/ json

--- serv.py
+++ serv.py
@@ -1,18 +1,25 @@
-import asyncio, websockets, nmap, jsonify
+import asyncio, websockets, nmap, json
 
 async def echo(websocket, path):
     async for message in websocket:
-        print(message,path)
-        nmScan=nmap.PortScanner()
-        print("scan begin")
-        devices = nmScan.scan("192.168.0.1/24","1")
-        ips=[]
-        for ip in devices["scan"]:
-            ips.append(ip)
-        print(ips)
-        await websocket.send(ips)
+        if (int(message) == 1)
+        print(message)
+       
+        await websocket.send(json.dumps(ips))
         print("scan end")
 
 asyncio.get_event_loop().run_until_complete(
     websockets.serve(echo, 'localhost', 8765))
 asyncio.get_event_loop().run_forever()
+
+
+#Return an array with every IP of connected devices (on local network)
+async def nmap_scan():
+    nmScan=nmap.PortScanner()
+    print("scan begin")
+    devices = nmScan.scan("192.168.0.1/24","1")
+    ips=[]
+    for ip in devices["scan"]:
+        ips.append(ip)
+    
+    return ips
