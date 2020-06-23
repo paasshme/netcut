79df527e05033e4cdcc99dc7d07ff88169b9796e
Clément Guiton <clement.guiton.dev@gmail.com>
2020-06-23 13:14:02 +0200

    tqt

--- serv.py
+++ serv.py
@@ -1,20 +1,7 @@
 import asyncio, websockets, nmap, json
 
-async def echo(websocket, path):
-    async for message in websocket:
-        if (int(message) == 1)
-        print(message)
-       
-        await websocket.send(json.dumps(ips))
-        print("scan end")
-
-asyncio.get_event_loop().run_until_complete(
-    websockets.serve(echo, 'localhost', 8765))
-asyncio.get_event_loop().run_forever()
-
-
 #Return an array with every IP of connected devices (on local network)
-async def nmap_scan():
+def nmap_scan():
     nmScan=nmap.PortScanner()
     print("scan begin")
     devices = nmScan.scan("192.168.0.1/24","1")
@@ -23,3 +10,17 @@ async def nmap_scan():
         ips.append(ip)
     
     return ips
+
+async def echo(websocket, path):
+    async for message in websocket:
+        # if (int(message) == 1)
+        print(message)
+       
+        await websocket.send(json.dumps(nmap_scan()))
+        print("scan end")
+
+asyncio.get_event_loop().run_until_complete(
+    websockets.serve(echo, 'localhost', 8765))
+asyncio.get_event_loop().run_forever()
+
+
