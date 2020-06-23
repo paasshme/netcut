62366930680da3b41c969517595274d1aa3b6120
PashmiDev <lowneonema@gmail.com>
2020-06-23 11:52:45 +0200

    fix

--- serv.py
+++ serv.py
@@ -1,19 +1,14 @@
-import asyncio
-import websockets
+import asyncio, websockets, nmap
 
 async def echo(websocket, path):
     async for message in websocket:
         print(message,path)
         nmScan=nmap.PortScanner()
-	    devices = nmScan.scan("192.168.0.1/23","1")
-
-	    ips=[]
-	    for ip in devices["scan"]:
-	    	ips.append(ip)
-
-    
-	    return jsonify(ips)
-            await websocket.send(message)
+        devices = nmScan.scan("192.168.0.1/23","1")
+        ips=[]
+        for ip in devices["scan"]:
+            ips.append(ip)
+        await websocket.send(jsonify(ips))
 
 asyncio.get_event_loop().run_until_complete(
     websockets.serve(echo, 'localhost', 8765))
