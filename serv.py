da59a8e58e1c4ce50a6a704553a9201ee76f3a6d
Clément Guiton <clement.guiton.dev@gmail.com>
2020-06-23 13:58:37 +0200

    controlleur

--- serv.py
+++ serv.py
@@ -13,10 +13,16 @@ def nmap_scan():
 
 async def echo(websocket, path):
     async for message in websocket:
-        # if (int(message) == 1)
-        print(message)
+        request = json.loads(message)
+        if request[0] == "scan":
+            
+        elif request[0] == "block":
+
+        
+        print(f'1 , {request[0]} \n')
+        print(f'2 , {request[1]}')
        
-        await websocket.send(json.dumps(nmap_scan()))
+        # await websocket.send(json.dumps(nmap_scan()))
         print("scan end")
 
 asyncio.get_event_loop().run_until_complete(
--- testClient.py
+++ testClient.py
@@ -1,11 +1,11 @@
-import asyncio
+import asyncio, json
 import websockets
 
 async def hello(uri):
 	async with websockets.connect(uri) as ws:
-		await ws.send("salut salut salut")
-		a=await ws.recv()
-		print(a)
+		await ws.send(json.dumps(("salut", "yo", "salut")))
+		# a=await ws.recv()
+		# print(a)
 
 asyncio.get_event_loop().run_until_complete(
     hello('ws://localhost:8765'))
