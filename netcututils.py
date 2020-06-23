1dbbe8ed2778ad6b5a9dbcc70a04e68aaaa59074
PashmiDev <lowneonema@gmail.com>
2020-06-23 14:03:57 +0200

    Can now choose which ip+mask to use

--- netcututils.py
+++ netcututils.py
@@ -1,25 +1,26 @@
 import nmap
 from scapy.all import *
 
-def nmap_scan():
+# Return IP & Mac of every connected device (Root might be useful be not necessary)
+def nmap_scan(target_ip= '192.168.0.0/24'):
     nmScan=nmap.PortScanner()
     print("scan begin")
-    devices = nmScan.scan("192.168.0.1/24","1")
+    devices = nmScan.scan(target_ip,"1")
     ips=[]
     for ip in devices["scan"]:
         ips.append(ip)
     
     return ips
 
-# Return IP & Mac of every connected client
-def arp_scan():
-    target_ip = "192.168.0.0/24"
+# Return IP & Mac of every connected client (Need root)
+def arp_scan(target_ip = '192.168.0.0/24'):
+    
     # IP Address for the destination
     # create ARP packet
     arp = ARP(pdst=target_ip)
     # create the Ether broadcast packet
     # ff:ff:ff:ff:ff:ff MAC address indicates broadcasting
-    ether = Ether(dst="ff:ff:ff:ff:ff:ff")
+    ether = Ether(dst='ff:ff:ff:ff:ff:ff')
     # stack them
     packet = ether/arp
 
@@ -43,4 +44,5 @@ def arp_scan():
 
 
 if __name__=='__main__':
+    arp_scan('192.168.2.0/24')
     arp_scan()
